5f2d94d79f5207c1dc36bb968d172dea86e80c8a
Stefano Woerner <mail@stefano-woerner.de>
2018-05-20 20:40:39 +0200

    moved checkpoint file to data folder

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
 wget -c http://kitti.is.tue.mpg.de/kitti/data_scene_flow.zip
 unzip data_scene_flow.zip
+
+wget -c http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+tar xvzf vgg_16_2016_08_28.tar.gz
+
