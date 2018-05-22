cb08b6e34530abb7d86f96945eb9718abc19b159
Stefano Woerner <mail@stefano-woerner.de>
2018-05-22 14:48:16 +0200

    introduced command line flags for the range and step size, as well as a debug mode

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ from models.VGG16FlowSearch import VGG16FlowSearch
 
 root = os.path.dirname(os.path.realpath(__file__))
 
-# Configuration
+# Config
+tf.flags.DEFINE_boolean('debug', False, 'Debug mode')
 tf.flags.DEFINE_string('image1', 'data/training/image_2/000055_10.png', 'Image 1')
 tf.flags.DEFINE_string('image2', 'data/training/image_2/000055_11.png', 'Image 2')
+tf.flags.DEFINE_integer('ymin', -64, 'Disparity range minimum in y-Direction')
+tf.flags.DEFINE_integer('ymax', 64, 'Disparity range maximum in y-Direction')
+tf.flags.DEFINE_integer('xmin', -64, 'Disparity range minimum in x-Direction')
+tf.flags.DEFINE_integer('xmax', 64, 'Disparity range maximum in x-Direction')
+tf.flags.DEFINE_integer('ystep', 16, 'Disparity block size in y-Direction')
+tf.flags.DEFINE_integer('xstep', 16, 'Disparity block size in x-Direction')
 
 
 def cart2pol(x, y):
@@ -25,12 +32,17 @@ def vis_flow(image):
     return hsv2rgb(np.stack((phi, rho, np.ones_like(rho)), axis=-1))
 
 def main(_):
-    config = tf.flags.FLAGS.__flags.copy()
+    FLAGS = tf.flags.FLAGS
+    if FLAGS.debug:
+        for k, v in FLAGS.__flags.items():
+            print("{}: {}".format(k.upper(), v.value))
+        print("")
 
     model = VGG16FlowSearch()
-    im1 = imread(os.path.join(root, tf.flags.FLAGS.image1))
-    im2 = imread(os.path.join(root, tf.flags.FLAGS.image2))
-    flow = model.infer(im1, im2)
+    im1 = imread(os.path.join(root, FLAGS.image1))
+    im2 = imread(os.path.join(root, FLAGS.image2))
+    flow = model.infer(im1, im2, d_range=[[FLAGS.ymin,FLAGS.ymax],[FLAGS.xmin,FLAGS.xmax]],
+                       step=[FLAGS.ystep,FLAGS.xstep])
 
     plt.figure(figsize=(26, 6))
     plt.subplot(1, 2, 1)
