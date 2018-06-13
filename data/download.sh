630e8cabd5451a397443fe76b50bb86c34bcf370
Vasily V <vasily@gmail.com>
2018-06-13 20:43:42 +0200

    new kitti url

--- data/download.sh
+++ data/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -c http://kitti.is.tue.mpg.de/kitti/data_scene_flow.zip
+wget -c https://s3.eu-central-1.amazonaws.com/avg-kitti/data_scene_flow.zip
 unzip data_scene_flow.zip
 
 wget -c http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
